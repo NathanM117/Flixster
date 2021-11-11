5aa14200bea2b4b832cd2f39b89707440d6fae8f
NathanM117 <36217116+NathanM117@users.noreply.github.com>
2021-11-10 23:49:08 -0500

    Unit 2 Updates

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by yupyup on 11/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
